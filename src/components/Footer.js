483daa8acb1bb25e1be11fe08896e5931989a822
Zach <zach@sprinterconsulting.com>
2019-10-17 18:56:40 -0500

    Added social media bar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import InstagramFeed from './InstagramFeed'
 import './Footer.css'
+import { SocialIcon } from 'react-social-icons';
 
 export default () => (
   <div>
@@ -12,9 +13,15 @@ export default () => (
     <InstagramFeed count="8" />
     <footer className="footer">
       <div className="container taCenter">
-        <span>
+        <div className="socialBar">
+          <SocialIcon url="https://instagram.com/sprinterconsulting/" />
+          <SocialIcon url="https://www.linkedin.com/company/sprinterconsulting" />
+          <SocialIcon url="https://twitter.com/Sprinter_HQ" />
+          <SocialIcon url="https://facebook.com" />
+        </div>
+        <div className="copyright">
           © Copyright {new Date().getFullYear()} All rights reserved.
-        </span>
+        </div>
       </div>
     </footer>
   </div>
