7f14560673345f84dc29d16926ec8b02d530e005
Zach <zach@sprinterconsulting.com>
2019-10-28 15:04:11 -0500

    and another

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -43,22 +43,6 @@ CMS.registerPreviewTemplate('posts', ({ entry }) => (
   <SinglePostTemplate {...entry.toJS().data} />
 ))
 
-
-
-var AboutPreview = createClass({
-  render: function() {
-    entry = this.props.entry;
-    var imageA = entry.getIn(['data', 'imageA']);
-    var bg = this.props.getAsset(imageA);
-    return h('div', {},
-    h('h1', {}, entry.getIn(['data', 'title'])),
-    h('img', {src: bg.toString()}),
-    h('div', {"className": "whatWeDo"}, this.props.widgetFor('whatWeDoBody'))
-    )
-  }
-});
-
-
-CMS.registerPreviewTemplate("about-us-page", AboutPreview)
-
-//CMS.registerPreviewTemplate('about-us-page', AboutPagePreview)
+CMS.registerPreviewTemplate('about-us-page', ({ entry }) => (
+  <AboutUsPageTemplate {...entry.toJS().data} />
+))
